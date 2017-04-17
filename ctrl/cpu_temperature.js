6b865610877f89af6a9feb600b3cbde1c7a97edd
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-04-17 17:43:06 +0100

    Placeholder command for getting temperature.

--- ctrl/cpu_temperature.js
+++ ctrl/cpu_temperature.js
@@ -1,4 +1,5 @@
 var CpuTemperatureDb = require("../db/cpu_temperature");
+var exec = require('child_process').exec;
 
 module.exports = (db) => {
 
@@ -9,16 +10,29 @@ module.exports = (db) => {
     return d.getTime();
   }
 
+  function getCPUTemperature() {
+    return new Promise((resolve, reject) => {
+      exec("cat /sys/class/thermal/thermal_zone0/temp", function(stderr, stdout) {
+        if (stdout) {
+          var temperatureCelcius = parseInt(stdout) / 1000;
+          resolve(temperatureCelcius);
+        } else {
+          reject("Unable to get CPU temperature. Error was: " + stderr);
+        }
+      });
+    });
+  }
+
   return {
     writeCurrentCpuTemperature: () => {
-      var currentTimeMillis = getCurrentTimeMillis();
 
-      // Hardcoded for now
-      var temperatureCelcius = 60;
-
-      return cpuTemperatureDb.writeTemperature(currentTimeMillis, temperatureCelcius).catch(error =>
+      getCPUTemperature().then(cpuTemperatureCelcius => {
+        var currentTimeMillis = getCurrentTimeMillis();
+        return cpuTemperatureDb.writeTemperature(currentTimeMillis, cpuTemperatureCelcius)
+      }).catch(error =>
         console.error("Error writing temperature to database. Error was: " + error)
       );
+
     }
   }
 

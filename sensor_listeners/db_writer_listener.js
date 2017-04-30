9d3cc594ad863fed38cc2ca897fbedcd8d7a4448
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-04-30 16:42:33 +0100

    Write temperature events to database only every minute.

--- sensor_listeners/db_writer_listener.js
+++ sensor_listeners/db_writer_listener.js
@@ -3,12 +3,29 @@ var config = require("../config.json")
 
 module.exports = (cpuDb) => {
 
+  /* Only handle messages for a topic every 'pollEvery' milliseconds */
+  function subscribeLimitedMessages(topic, handler, pollEvery) {
+
+    var lastHandled = new Date().getTime();
+
+    PubSub.subscribe(topic, function() {
+      var timeNowMillis = new Date().getTime();
+
+      if ( (timeNowMillis - lastHandled) >= pollEvery) {
+        handler();
+        lastHandled = new Date().getTime();
+      }
+
+    });
+
+  }
+
   function writeTemperatureEvent(time, temperatureCelcius) {
     cpuDb.writeTemperature(time, temperatureCelcius);
   }
 
   function storeSensorEvents() {
-    PubSub.subscribe(config.cpuTemperatureTopic, writeTemperatureEvent);
+    subscribeLimitedMessages(config.cpuTemperatureTopic, writeTemperatureEvent, config.db.storeFrequencyMillis);
   }
 
   return {

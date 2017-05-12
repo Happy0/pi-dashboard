f620c21d062c07e7036d6a793538e568395f32f0
Gordon Martin <gordon.hugh.martin@gmail.com>
2017-05-12 19:05:50 +0100

    Continue over engineering my toy project :P.

---
+++ ctrl/temp.js
@@ -0,0 +1,29 @@
+var PubSub = require('pubsub-js');
+var conf = require('../config.json');
+
+module.exports = (tempDb) => {
+
+  const recentTemperaturesCapacity = 200;
+  const recentTemperatures = [];
+
+  function keepRecentTemperaturesBufferUpdated() {
+    PubSub.subscribe(config.cpuTemperatureTopic, (msg, data) => {
+      if (recentTemperaturesCapacity >= recentTemperaturesCapacity) {
+        recentTemperatures.shift();
+      }
+
+      recentTemperatures.push(data);
+    });
+  }
+
+  function getRecentTemperatures(limit) {
+    return recentTemperatures.slice(-limit);
+  }
+
+  keepRecentTemperaturesBufferUpdated();
+
+  return {
+    getRecentTemperatures : getRecentTemperatures
+  }
+
+}
